125f543ff9b6c1c0f6885d2cc7488f2adb2a2900
MikeLutgeMG <mike.lutge@marquam.com>
2015-04-17 15:57:49 -0700

    bundling and minifying css and data

---
+++ WTA.Schedules/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WTA.Schedules
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/data/data").Include(
+                "~/data/calendar.js",
+                "~/data/calendar_dates.js",
+                "~/data/routes.js",
+                "~/data/stops.js",
+                "~/data/stop_times.min.js",
+                "~/data/trips.js"));
+
+            bundles.Add(new StyleBundle("~/css/css").Include(
+                "~/css/bootstrap.min.css",
+                "~/css/bootstrap-custom.css",
+                "~/css/font-awesome.min.css",
+                "~/css/jquery-ui.min.css",
+                "~/css/main.css"));
+        }
+    }
+}
--- WTA.Schedules/Global.asax.cs
+++ WTA.Schedules/Global.asax.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
-
+using System.Web.Optimization;
 
 namespace WTA.Schedules
 {
@@ -13,6 +13,7 @@ namespace WTA.Schedules
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
