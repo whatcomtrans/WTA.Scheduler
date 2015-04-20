2d7f9ece98b40add0b1d4446adc56d6fd8c86085
MikeLutgeMG <mike.lutge@marquam.com>
2015-04-20 10:51:29 -0700

    additional performance changes

--- WTA.Schedules/App_Start/BundleConfig.cs
+++ WTA.Schedules/App_Start/BundleConfig.cs
@@ -11,14 +11,26 @@ namespace WTA.Schedules
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // Core js
+            bundles.Add(new ScriptBundle("~/Scripts/core").Include(
+                "~/Scripts/jquery-1.9.0.min.js",
+                "~/Scripts/jquery-ui.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/linq.min.js",
+                "~/Scripts/main.js"));
+
+            // Data js
             bundles.Add(new ScriptBundle("~/data/data").Include(
                 "~/data/calendar.js",
                 "~/data/calendar_dates.js",
                 "~/data/routes.js",
                 "~/data/stops.js",
                 "~/data/stop_times.min.js",
-                "~/data/trips.js"));
+                "~/data/trips.js",
+                "~/data/notices.js"));
 
+
+            // CSS
             bundles.Add(new StyleBundle("~/css/css").Include(
                 "~/css/bootstrap.min.css",
                 "~/css/bootstrap-custom.css",
