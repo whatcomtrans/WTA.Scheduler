a83055fddbce5424c77ff2776b96eb4b48e1ee76
R. Josh Nylander <joshn@ridewta.com>
2015-07-30 15:23:44 -0700

    Worked on Issue #37.  Closed Issue 32.

---
+++ build.bat
@@ -0,0 +1,30 @@
+@echo Off
+Echo Using NODE.JS with UglifyJS installed
+goto %1
+Echo Minifying and combining Javascript application files
+cd WTA.Schedules
+cd Scripts
+uglifyjs jquery-ui.min.js linq.min.js main.js bootstrap.min.js -o app.min.js
+
+Echo Minifying and combining data set 1
+cd ..
+cd data
+uglifyjs routes.js calendar.js calendar_dates.js stops.js -o data_routes.js
+Echo Minifying and combining data set 1
+uglifyjs trips.js stop_times.min.js -o data_trips.js
+cd ..
+
+Echo Minifying and combining CSS
+cd css
+uglifycss bootstrap.min.css bootstrap-custom.css font-awesome.min.css jquery-ui.min.css main.css > combined.css
+
+goto end
+:install
+Echo Installing UglifyJS 2
+npm install uglify-js -g
+
+Echo Installing UglifyCSS
+npm install uglifycss -g
+
+:end
+cls
