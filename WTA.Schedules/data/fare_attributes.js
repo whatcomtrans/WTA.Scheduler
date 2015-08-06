cf56a87ae33d70789e7213d7c60a183a52c576e4
R. Josh Nylander <joshn@ridewta.com>
2015-08-06 13:49:07 -0700

    Moved "data" (except notices.js, for now) to http://data.ridewta.com/.

--- WTA.Schedules/data/fare_attributes.js
+++
@@ -1,4 +0,0 @@
-var fare_attributes = [
-{"fare_id":1,"price":1,"currency_type":"USD","payment_method":0,"transfers":0,"transfer_duration":""},
-{"fare_id":2,"price":2,"currency_type":"USD","payment_method":0,"transfers":0,"transfer_duration":""}
-]
