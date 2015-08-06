631faacdbd663d57f3c2fb5e3c0fa139883e2856
AndyM <andym@whatcomtrans.net>
2015-08-06 15:14:56 -0700

    Fixed iPad/iPhone bug
    There was an issue with the selector in some versions of mobile safari.

--- WTA.Schedules/Scripts/main.js
+++ WTA.Schedules/Scripts/main.js
@@ -587,9 +587,9 @@ function displaySelectedRoute() {
                 return new Date('1970/01/01 ' + a.departure_time) - new Date('1970/01/01 ' + b.departure_time);
             });
             var j = 0;
-           
+
             for (k = 0; k < uniqueStopNamesInRoute.length; k++) {  
-                var columnId = parseInt($('#stopNames td:nth-child(' + (k + 1) + ')').attr('id')); // This line is failing on iPad: Mike 3-24              
+                var columnId = parseInt($('#stopNames td:eq(' + (k) + ')').attr('id')); // This line is failing on iPad: Mike 3-24        
                 if (stopTimesInTrip[j] == undefined) {
                     $(tripId).append('<td id="' + uniqueStopNamesInRoute[k].stop_id + '">--</td>');
                 } else if (stopTimesInTrip[j].stop_id == parseInt($('#stopNames td:nth-child(' + (k) + ')').attr('id'))) {
